4f56132378459dde80fefc910ec1d8e8b6ba548d
Rafal <rglabus@gmail.com>
2026-06-14 14:34:29 +0200

    Naprawiono nazwę modelu na models/gemini-1.5-flash

--- receipt_scanner.py
+++ receipt_scanner.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="ScanerAI", page_icon="🧾")
 
 # Konfiguracja klienta
 genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-1.5-pro')
 
 def save_to_database(receipt_json):
     conn = sqlite3.connect('expanses.db')
